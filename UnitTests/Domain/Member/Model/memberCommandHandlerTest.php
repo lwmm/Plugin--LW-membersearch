79505993fd2cdcddd36b01667e408afce4c8645b
Dr. Andreas Eckhoff <eckhoff@logicnetwork.de>
2013-01-17 11:33:56 +0100

    initial commit

--- UnitTests/Domain/Member/Model/memberCommandHandlerTest.php
+++ UnitTests/Domain/Member/Model/memberCommandHandlerTest.php
@@ -39,68 +39,68 @@ class memberCommandHandlerTest extends \PHPUnit_Framework_TestCase {
          */
     }
     
-    public function testAddMemberEmptyArray($array)
+    public function testAddMember_EmptyArray($array)
     {
         // check if add member throws Exception, if an empty Array was given
     }
     
-    public function testAddMemberFullValidArray($array)
+    public function testAddMember_FullValidArray($array)
     {
         // check if add member works, with a full and valid Array
     }
     
-    public function testAddMemberInValidArray($array)
+    public function testAddMember_InValidArray($array)
     {
         // check if add member throws Exception, if an invalid array was given
     }
     
-    public function testAddMemberMinimalValidArray($array)
+    public function testAddMember_MinimalValidArray($array)
     {
         // check if add member works, if a minimal valid array was given
         // minimal means: at least name is given and the automatic first/last date was saved
     }
     
-    public function testSaveMemberEmptyArray($id, $array)
+    public function testSaveMember_EmptyArray($id, $array)
     {
         // check if save member throws Exception, if an empty Array was given
     }
     
-    public function testSaveMemberFullValidArray($id, $array)
+    public function testSaveMember_FullValidArray($id, $array)
     {
         // check if save member works, with a full and valid Array
     }
     
-    public function testSaveMemberInValidArray($id, $array)
+    public function testSaveMember_InValidArray($id, $array)
     {
         // check if save member throws Exception, if an invalid array was given
     }
     
-    public function testSaveMemberMinimalValidArray($id, $array)
+    public function testSaveMember_MinimalValidArray($id, $array)
     {
         // check if save member works, if a minimal valid array was given
         // minimal means: at least the id is given and the automatic first/last date was saved
     }
     
-    public function testDeleteMemberWithExistingId($id)
+    public function testDeleteMember_WithExistingId($id)
     {
         // add a new member and put ID in variable (id must be 1)
         // delete member with the given $id (which should be 1)
         // check if delete works (member with id is deleted
     }
     
-    public function testDeleteMemberWithNotExistingId($id)
+    public function testDeleteMember_WithNotExistingId($id)
     {
         // add a new member and put ID in variable (id must be 1)
         // delete member with an $id other than 1
         // check if delete throws an exception, telling the given ID doesn't exist
     }
     
-    public function testDeleteMemberWithIdIsFalse($id)
+    public function testDeleteMember_WithIdIsFalse($id)
     {
         // check if delete throws an exception, telling that no ID was given
     }
 
-    public function testDeleteMemberWithIdIsNotNumeric($id)
+    public function testDeleteMember_WithIdIsNotNumeric($id)
     {
         // check if delete throws an exception, telling that the given ID is not numeric
     }
