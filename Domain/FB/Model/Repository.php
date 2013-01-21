feaddea954e5eafdf75bb341baf144673eabef4f
Dr. Andreas Eckhoff <eckhoff@logicnetwork.de>
2013-01-21 17:03:36 +0100

    moved object factory methods to parent LWddd class

--- Domain/FB/Model/Repository.php
+++ Domain/FB/Model/Repository.php
@@ -7,54 +7,7 @@ class Repository extends \LWddd\Repository
     public function __construct()
     {
         parent::__construct();
-    }
-    
-    protected function getCommandHandler()
-    {
-        if (!$this->commandHandler) {
-            $this->commandHandler = new CommandHandler($this->dic->getDbObject());
-        }
-        return $this->commandHandler;
-    }
-    
-    protected function getQueryHandler()
-    {
-        if (!$this->queryHandler) {
-            $this->queryHandler = new QueryHandler($this->dic->getDbObject());
-        }
-        return $this->queryHandler;
-    }
-    
-    protected function getIsValidSpecification()
-    {
-        if (!$this->isValidSpecification) {
-            $this->isValidSpecification = \lwMembersearch\Domain\FB\Specification\isValid::getInstance();
-        }
-        return $this->isValidSpecification;
-    }
-    
-    protected function getFactory()
-    {
-        if (!$this->factory) {
-            $this->factory = \lwMembersearch\Domain\FB\Model\Factory::getInstance();
-        }
-        return $this->factory;
-    }
-    
-    protected function getIsDeletableSpecification()
-    {
-        if (!$this->isDeletableSpecification) {
-            $this->isDeletableSpecification = \lwMembersearch\Domain\FB\Specification\isDeletable::getInstance();
-        }
-        return $this->isDeletableSpecification;
-    }
-    
-    protected function getDataValueObjectFilter()
-    {
-        if (!$this->DataValueObjectFilter) {
-            $this->DataValueObjectFilter = $this->dic->getFbFilter();
-        }
-        return $this->DataValueObjectFilter;
+        $this->baseNamespace = "lwMembersearch\\Domain\\FB\\";
     }
     
     protected function buildObjectById($id)
